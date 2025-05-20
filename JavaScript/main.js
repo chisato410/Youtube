4ffb38b05f9581efe083f760179277b678317918
chisato410 <it242243@trident.ac.jp>
2025-05-20 10:39:20 +0900

    修正

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,7 +2,6 @@ import { initPlayer } from "./videoPlayer.js";
 import { setupEventListeners } from "./ui.js";
 import { loadNotes } from "./noteManager.js";
 import { setupSearch } from "./search.js";
-import { fallbackVideos } from "./fallbackVideos.js";
 
 window.onload = () => {
   initPlayer(() => {
